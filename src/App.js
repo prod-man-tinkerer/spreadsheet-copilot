cf70a77370886be989943eb5ebea423863854576
Siddharth Vora <sivora@microsoft.com>
2025-04-03 23:15:28 +0530

    fortune-sheet working prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Workbook } from '@fortune-sheet/react';
+import '@fortune-sheet/react/dist/index.css';
 import './App.css';
 
 function App() {
+  const data = [
+    {
+      name: "Sheet1",
+      celldata: [],
+      row: 100,
+      column: 100,
+      config: {
+        rowHeightDefault: 25,
+        colWidthDefault: 80,
+      }
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ height: '100vh', width: '100%' }}>
+        <Workbook 
+          data={data}
+          options={{
+            showToolbar: true,
+            showGrid: true,
+            showFormulaBar: true,
+          }}
+          onChange={(sheetData) => {
+            console.log('Sheet data changed:', sheetData);
+          }}
+        />
+      </div>
     </div>
   );
 }
